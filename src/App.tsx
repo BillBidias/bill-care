7651198b838f33f85a5a5a877d689eb3d239abe1
BillBidias <bidiasbill83@gmail.com>
2026-08-30 11:28:10 +0200

    Route training videos page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ProgramFinderPage from "./pages/ProgramFinderPage.tsx";
 import AboutPage from "./pages/AboutPage.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import ExamplePage from "./pages/ExamplePage.tsx";
+import TrainingVideosPage from "./pages/TrainingVideosPage.tsx";
 import PurchaseSimulationPage from "./pages/PurchaseSimulationPage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
@@ -53,6 +54,7 @@ const App = () => (
                   <Route path="/finder" element={<ProgramFinderPage />} />
                   <Route path="/quiz" element={<ProgramFinderPage />} />
                   <Route path="/programs" element={<ProgramsPage />} />
+                  <Route path="/training-videos" element={<TrainingVideosPage />} />
                   <Route path="/purchase-simulation" element={<PurchaseSimulationPage />} />
                   <Route path="/cart" element={<CartPage />} />
                   <Route path="/about" element={<AboutPage />} />
